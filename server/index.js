9ce19aac658a08542e5479a9e9d8869f07a54b9b
JackW25 <wonnacottjack25@gmail.com>
2022-03-09 20:34:01 -0800

    Working Buttons, Responsive Web Layout

--- server/index.js
+++ server/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const ytdl = require('ytdl-core');
 const app = express();
-app.use('/static', express.static('./static'));
 
+app.use('/static', express.static('./static'));
 app.listen(3000, () => {
     console.log("Website Online!");
 });
@@ -12,8 +12,20 @@ app.get('/', (req, res) => {
     res.sendFile('index.html', { root: './' });
 })
 
-app.get('/download', (req, res) => {
+app.get('/mp4', (req, res) => {
     var url = req.query.url;
     res.header("Content-Disposition", 'attachment; filename="Video.mp4');
     ytdl(url, {format: 'mp4'}).pipe(res);
+});
+
+app.get('/mp3', (req, res) => {
+    var url = req.query.url;
+    res.header("Content-Disposition", 'attachment; filename="Audio.mp3');
+    ytdl(url, {format: 'mp3'}).pipe(res);
+});
+
+app.get('/wav', (req, res) => {
+    var url = req.query.url;
+    res.header("Content-Disposition", 'attachment; filename="Waveform.wav');
+    ytdl(url, {format: 'wav'}).pipe(res);
 });
