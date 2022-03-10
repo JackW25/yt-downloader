13ac4a7149f2b8412607780b63fb5af8408b0348
JackW25 <wonnacottjack25@gmail.com>
2022-03-10 15:42:43 -0800

    Update index.js

--- index.js
+++ index.js
@@ -15,19 +15,19 @@ app.get('/', function(request, response) {
 })
 
 app.get('/mp4', (request, response) => {
-    var url = req.query.url;
+    var url = request.query.url;
     response.header("Content-Disposition", 'attachment; filename="Video.mp4');
     ytdl(url, {format: 'mp4'}).pipe(res);
 });
 
 app.get('/mp3', (request, response) => {
-    var url = req.query.url;
+    var url = request.query.url;
     response.header("Content-Disposition", 'attachment; filename="Audio.mp3');
     ytdl(url, {format: 'mp3'}).pipe(res);
 });
 
 app.get('/wav', (request, response) => {
-    var url = req.query.url;
+    var url = request.query.url;
     response.header("Content-Disposition", 'attachment; filename="Waveform.wav');
     ytdl(url, {format: 'wav'}).pipe(res);
 });
