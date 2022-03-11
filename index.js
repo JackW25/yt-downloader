321428ffe0ed06419f8d3b3e7ad0a20d43fb551c
JackW25 <wonnacottjack25@gmail.com>
2022-03-10 19:53:48 -0800

    Added Additional Formats

--- index.js
+++ index.js
@@ -31,3 +31,21 @@ app.get('/wav', (request, response) => {
     response.header("Content-Disposition", 'attachment; filename="Waveform.wav');
     ytdl(url, {format: 'wav'}).pipe(response);
 });
+
+app.get('/ogg', (request, response) => {
+    var url = request.query.url;
+    response.header("Content-Disposition", 'attachment; filename="Vorbis.ogg');
+    ytdl(url, {format: 'ogg'}).pipe(response);
+});
+
+app.get('/webm', (request, response) => {
+    var url = request.query.url;
+    response.header("Content-Disposition", 'attachment; filename="Opus.webm');
+    ytdl(url, {format: 'webm'}).pipe(response);
+});
+
+app.get('/flv', (request, response) => {
+    var url = request.query.url;
+    response.header("Content-Disposition", 'attachment; filename="Flash.flv');
+    ytdl(url, {format: 'flv'}).pipe(response);
+});
