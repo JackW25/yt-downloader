9e61d1b76f3b2f9578460f40d8d1c1bad868d03b
JackW25 <wonnacottjack25@gmail.com>
2022-03-11 17:57:38 -0800

    Icon | More Logging

--- index.js
+++ index.js
@@ -12,40 +12,47 @@ app.listen(PORT, () => {
 
 app.get('/', function(request, response) {
     response.sendFile('index.html', { root: './' });
-})
-
+    console.log('Homepage Landed');
+});
+    
 app.get('/mp4', (request, response) => {
     var url = request.query.url;
     response.header("Content-Disposition", 'attachment; filename="Video.mp4');
     ytdl(url, {format: 'mp4'}).pipe(response);
+    console.log(url);
 });
 
 app.get('/mp3', (request, response) => {
     var url = request.query.url;
     response.header("Content-Disposition", 'attachment; filename="Audio.mp3');
     ytdl(url, {format: 'mp3'}).pipe(response);
+    console.log(`${url}.mp3`);
 });
 
 app.get('/wav', (request, response) => {
     var url = request.query.url;
     response.header("Content-Disposition", 'attachment; filename="Waveform.wav');
     ytdl(url, {format: 'wav'}).pipe(response);
+    console.log(`${url}.wav`);
 });
 
 app.get('/ogg', (request, response) => {
     var url = request.query.url;
     response.header("Content-Disposition", 'attachment; filename="Vorbis.ogg');
     ytdl(url, {format: 'ogg'}).pipe(response);
+    console.log(`${url}.ogg`);
 });
 
 app.get('/webm', (request, response) => {
     var url = request.query.url;
     response.header("Content-Disposition", 'attachment; filename="Opus.webm');
     ytdl(url, {format: 'webm'}).pipe(response);
+    console.log(`${url}.webm`);
 });
 
 app.get('/flv', (request, response) => {
     var url = request.query.url;
     response.header("Content-Disposition", 'attachment; filename="Flash.flv');
     ytdl(url, {format: 'flv'}).pipe(response);
+    console.log(`${url}.flv`);
 });
